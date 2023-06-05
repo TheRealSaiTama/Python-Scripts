ec27e6cb8807534d699bd86db048a536585482b4
LightIce <110764833+TheRealSaiTama@users.noreply.github.com>
2023-06-05 18:05:18 +0000

    Add files via upload

---
+++ whatsappautomation.py
@@ -0,0 +1,7 @@
+import pywhatkit 
+
+phone_number = "+918586087339"
+message = "Happy Birthday to the most carefree girl i've ever met!"
+hour = 0
+minute = 0
+pywhatkit.sendwhatmsg(phone_number,message,hour,minute)
