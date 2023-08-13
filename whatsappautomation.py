798377b17098b9cd24fb1929dc525542164a1278
LightIce <110764833+TheRealSaiTama@users.noreply.github.com>
2023-08-13 15:42:44 +0530

    Update whatsappautomation.py

--- whatsappautomation.py
+++ whatsappautomation.py
@@ -1,7 +1,7 @@
 import pywhatkit 
 
 phone_number = "+91XXXXXXXXXX"
-message = "Happy Birthday to the most carefree girl i've ever met!"
+message = "Happy Birthday"
 hour = 0
 minute = 0
 pywhatkit.sendwhatmsg(phone_number,message,hour,minute)
